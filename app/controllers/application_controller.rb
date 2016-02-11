e1317f66f8a037a8b049f45976cb9d144f88858c
David P. Hanesana <dhanesana@gmail.com>
2016-02-10 22:09:39 -0800

    Add User authentication to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,48 @@
 class ApplicationController < ActionController::Base
+  include AbstractController::Translation
+
+  before_action :authenticate_user_from_token!
+
   # Inherit from ActionController::API and serve only JSON
   respond_to :json
+
+  # User Authentication
+  # Authenticate the user with OAuth2 Resource Owner Password Credentials Grant
+  def authenticate_user_from_token!
+    auth_token = request.headers['Authorization']
+    if auth_token
+      authenticate_with_auth_token auth_token
+    else
+      authentication_error
+    end
+  end
+
+  private
+
+  def authenticate_with_auth_token auth_token
+    unless auth_token.include?(':')
+      authentication_error
+      return
+    end
+
+    user_id = auth_token.split(':').first
+    user = User.where(id: user_id).first
+
+    if user && Devise.secure_compare(user.access_token, auth_token)
+      # User can access
+      sign_in user, store: false
+    else
+      authentication_error
+    end
+  end
+
+  # Authentication Failure
+  # Render 401 Error
+  def authentication_error
+    # User's token is either invalid or not in the right format
+    render json: {error: t('unauthorized')}, status 401 # Authentication timeout
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
